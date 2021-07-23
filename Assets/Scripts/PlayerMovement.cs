b59f714c07f787dabadddd060286d74837be39aa
lluispalerm <48449992+lluispalerm@users.noreply.github.com>
2021-07-23 13:08:51 +0200

    Movimento Temporal TM

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement parameters")]
     public float walkVelocity;
     public float movementForce;
     public bool drawDirection;
     public float velocityLimit;
+
+    [Header("Jump parameters")]
+    public float jumpForce;
+    public float coyoteTime;
+    public float maxJumpTime;
+    private float airTime;
+    private float jumpTime;
+    public float castGroundDistance;
+    public float sphereCastRadious;
+    public LayerMask groundMask;
 
     public enum PandaState { Walking, Rolling}
     public PandaState pandaState;
@@ -17,6 +28,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 direction;
     private Vector2 inputMovement;
     private Rigidbody rb;
+    private bool jumpPressed;
+    private bool grounded;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
     {
         direction = new Vector3(inputMovement.x, 0, inputMovement.y);
 
+        //Movimiento
         switch (pandaState) {
             case PandaState.Walking:
                 Vector3 aux = transform.localPosition + direction * walkVelocity * Time.deltaTime;
@@ -40,11 +55,51 @@ public class PlayerMovement : MonoBehaviour
                 break;
         }
 
+        // grounded
+        RaycastHit hitInfo;
+
+        if (grounded = Physics.SphereCast(transform.position + Vector3.up * 0.12f, sphereCastRadious, Vector3.down, out hitInfo, castGroundDistance, groundMask))
+        {
+            airTime = 0;
+            jumpTime = 0;
+        }
+        else
+            airTime += Time.deltaTime;
+
+        //Salto
+        if (jumpPressed && jumpTime < maxJumpTime) {
+            if (grounded || airTime < coyoteTime)
+            {
+                float jumpVelocity = jumpForce * (rb.velocity.magnitude / velocityLimit);
+                //rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
+                rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, rb.velocity.z);
+                jumpTime += Time.deltaTime;
+            }
+        }
+
+
+        //Limitadores de velocidad
+        if (rb.velocity.magnitude >= velocityLimit) rb.velocity = rb.velocity.normalized * velocityLimit;
+
+        if (inputMovement == Vector2.zero && rb.velocity.magnitude <= 1f && grounded) rb.velocity = Vector3.zero;
+
+        // Lineas de debug
         if (drawDirection && direction != Vector3.zero)
         {
             Debug.DrawLine(transform.position, transform.position + direction * 5, Color.red);
         }
+
+       
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position + Vector3.up * 0.12f , Vector3.down * castGroundDistance);
+        
+    }
+
+
 
     public void Move(InputAction.CallbackContext context)
     {
@@ -53,7 +108,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValueAsObject());
+        jumpPressed = context.ReadValue<float>() != 0;
     }
 
     public void ChangeMode(InputAction.CallbackContext context)
@@ -61,4 +116,6 @@ public class PlayerMovement : MonoBehaviour
         if (pandaState == PandaState.Walking) pandaState = PandaState.Rolling;
         else pandaState = PandaState.Walking;
     }
+
+
 }
